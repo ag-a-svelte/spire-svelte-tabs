c85048521e1f094753ebea9678530a0251095cf5
Jitesh Doshi <jitesh@spinspire.com>
2020-08-15 22:34:14 -0400

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { TabGroup, Tab, TabContent } from "./tabs";
+  const tabs = [
+    ["One", "Content of tab one. Foo. Foo. Foo."],
+    ["Two", "Content of tab two. Bar. Bar. Bar."],
+  ];
+</script>
+
+<main>
+  <div class="center">
+    <img src="favicon.png" alt="favicon" />
+    <h1>Svelte App</h1>
+    <h2>SpinSpire</h2>
+  </div>
+  <TabGroup active="foo">
+    <div slot="tabs">
+      {#each tabs as [title, body], index}
+        <Tab key={title}>{title}</Tab>
+      {/each}
+      <Tab key="foo">Third</Tab>
+    </div>
+    {#each tabs as [title, body], index}
+      <TabContent key={title}>{body}</TabContent>
+    {/each}
+    <TabContent key="foo">Contents of third tab.</TabContent>
+  </TabGroup>
+</main>
---
+++ src/tabs/Tab.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  export let key: string | number;
+  import { getContext } from "svelte";
+  import type { Writable } from "svelte/store";
+  const store: Writable<string | number> = getContext("activeTab");
+</script>
+
+<style>
+  .title {
+    /* border: solid lightgray 2px; */
+    border-bottom: none;
+    display: inline-block;
+    padding: 1em;
+    color: var(--tab-color-fg, black);
+    background-color: var(--tab-color-bg, white);
+  }
+  .title.active {
+    color: var(--tab-color-active-fg, white);
+    background-color: var(--tab-color-active-bg, black);
+  }
+  .title:not(.active):hover {
+    cursor: pointer;
+    color: var(--tab-color-hover-fg, black);
+    background-color: var(--tab-color-hover-bg, lightgray);
+  }
+</style>
+
+<div
+  class="title"
+  on:click={() => ($store = key)}
+  class:active={$store === key}>
+  <slot />
+</div>
